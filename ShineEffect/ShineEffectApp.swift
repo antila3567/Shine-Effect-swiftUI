7e60cbd8c3e232d51c7f5952b3b18898cf37c2e8
IvanL <antila3567@gmail.com>
2023-12-27 12:47:34 +0200

    Initial Commit

---
+++ ShineEffect/ShineEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShineEffectApp.swift
+//  ShineEffect
+//
+//  Created by Иван Легенький on 27.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShineEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
